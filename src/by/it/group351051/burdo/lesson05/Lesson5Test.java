c492024f30809270c34176735048a496bffdcd87
ultraMaria <mar.bur@tutamail.com>
2024-11-30 14:49:42 +0300

    Lesson 5B

--- src/by/it/group351051/burdo/lesson05/Lesson5Test.java
+++ src/by/it/group351051/burdo/lesson05/Lesson5Test.java
@@ -29,4 +29,26 @@ public class Lesson5Test {
         assertTrue("A failed", ok);
     }
 
+
+    @Test
+    public void B1() throws Exception {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/group351051/burdo/lesson05/dataB.txt");
+        B_CountSort instance = new B_CountSort();
+        int[] result=instance.countSort(stream);
+        boolean ok=Arrays.equals(result,new int[]{2,2,3,9,9});
+        assertTrue("B failed", ok);
+    }
+
+    @Test
+    public void B2() throws Exception {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/group351051/burdo/lesson05/dataBTest.txt");
+        B_CountSort instance = new B_CountSort();
+        int[] result=instance.countSort(stream);
+        boolean ok=Arrays.equals(result,new int[]{1,2,2,3,4,9,9,88,999});
+        assertTrue("B failed", ok);
+    }
+
+
 }
