37aeb67e431e878c0aad4e74d40a4034a4c0976b
ultraMaria <mar.bur@tutamail.com>
2024-12-04 02:03:45 +0300

    Lesson 6C

--- src/by/it/group351051/burdo/lesson06/Lesson6Test.java
+++ src/by/it/group351051/burdo/lesson06/Lesson6Test.java
@@ -48,4 +48,24 @@ public class Lesson6Test {
         assertTrue("B failed", ok);
     }
 
+    @Test(timeout = 1000)
+    public void C1() throws Exception {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/group351051/burdo/lesson06/dataC.txt");
+        C_LongNotUpSubSeq instance = new C_LongNotUpSubSeq();
+        int result=instance.getNotUpSeqSize(stream);
+        boolean ok=(result==4);
+        assertTrue("C failed", ok);
+    }
+
+    @Test(timeout = 1000)
+    public void C2() throws Exception {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/group351051/burdo/lesson06/dataCTest.txt");
+        C_LongNotUpSubSeq instance = new C_LongNotUpSubSeq();
+        int result=instance.getNotUpSeqSize(stream);
+        boolean ok=(result==6);
+        assertTrue("C failed", ok);
+    }
+
 }
