3a155a7c921065351216f15c6110fbdff1a3f26a
artyomnosovich <artyomnosovich@gmail.com>
2024-12-07 15:06:58 +0300

    Lesson1 C

--- src/by/it/group351051/nosovich/lesson01/FiboC.java
+++ src/by/it/group351051/nosovich/lesson01/FiboC.java
@@ -16,18 +16,50 @@ public class FiboC {
 
     public static void main(String[] args) {
         FiboC fibo = new FiboC();
-        int n = 10;
-        int m = 2;
+        int n = 7704;
+        int m = 485;
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
 
     long fasterC(long n, int m) {
-        //Решение сложно найти интуитивно
-        //возможно потребуется дополнительный поиск информации
-        //см. период Пизано
-        return 0L;
+        // Нахождение периода Пизано
+        long pisanoPeriod = pisanoPeriodLength(m);
+        // Нахождение остатка от деления n на длину периода для сокращения вычислений
+        n = n % pisanoPeriod;
+        // Нахождение n-го числа Фибоначчи по модулю m
+        return fibonacciMod(n, m);
     }
 
+    private long pisanoPeriodLength(int m) {
+        long previous = 0;
+        long current = 1;
+        for (long i = 0; i < m * m; i++) {
+            long temp = current; // Временное = текущее
+            current = (previous + current) % m; //Текущее равно остаток от деления (пред. + тек.) на m
+            previous = temp; // Предыдущее = временное (текущее до расчетов)
 
+            // Период заканчивается, когда значение предыдущего = 0 и текущего = 1
+            // В случае с m = 485 при достижении current 486 previous = 0, а current = 1
+            if (previous == 0 && current == 1) {
+                return i + 1;
+            }
+        }
+        return -1; // Не должно быть достигнуто
+    }
+
+    private long fibonacciMod(long n, int m) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        long previous = 0;
+        long current = 1;
+
+        for (long i = 2; i <= n; i++) {
+            long temp = current;
+            current = (previous + current) % m;
+            previous = temp;
+        }
+
+        return current;
+    }
 }
-
