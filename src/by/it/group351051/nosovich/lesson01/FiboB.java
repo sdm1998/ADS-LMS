7ed0fafafeaef84067331576fb4e36a7d6c135b7
artyomnosovich <artyomnosovich@gmail.com>
2024-12-07 15:06:24 +0300

    Lesson 1 B

--- src/by/it/group351051/nosovich/lesson01/FiboB.java
+++ src/by/it/group351051/nosovich/lesson01/FiboB.java
@@ -25,8 +25,21 @@ public class FiboB {
 
     BigInteger fastB(Integer n) {
         //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
+        // Проверка на базовые случаи
+        if (n == 0) return BigInteger.ZERO;
+        if (n == 1) return BigInteger.ONE;
 
-            return BigInteger.ZERO;
+        // Создание массива для хранения чисел Фибоначчи
+        BigInteger[] fib = new BigInteger[n+1];
+        // Начальные значения
+        fib[0] = BigInteger.ZERO;
+        fib[1] = BigInteger.ONE;
+        // Заполнение массива
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i-1].add(fib[i-2]);
+        }
+        // Возвращение числа Фибоначчи
+            return fib[n];
     }
 
 }
