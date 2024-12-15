c5df696628810f7669c43f0e0968b7c6e23fbf23
artyomnosovich <artyomnosovich@gmail.com>
2024-12-15 18:43:24 +0300

    Lesson4 B

--- src/by/it/group351051/nosovich/lesson04/B_MergeSort.java
+++ src/by/it/group351051/nosovich/lesson04/B_MergeSort.java
@@ -29,23 +29,68 @@ public class B_MergeSort {
         //размер массива
         int n = scanner.nextInt();
         //сам массив
-        int[] a=new int[n];
+        int[] a = new int[n];
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
             System.out.println(a[i]);
         }
-
-        // тут ваше решение (реализуйте сортировку слиянием)
-        // https://ru.wikipedia.org/wiki/Сортировка_слиянием
-
-
-
-
-
-
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        // Вызов метода и сортировка с помощью его
+        mergeSort(a, 0, n - 1);
         return a;
     }
+        // тут ваше решение (реализуйте сортировку слиянием)
+        // https://ru.wikipedia.org/wiki/Сортировка_слиянием\
+    // Метод для сортировки слиянием
+    private void mergeSort(int[] array, int left, int right) {
+            if (left < right) {
+                int mid = (left + right) / 2;
+                // Рекурсивная сортировка первой и второй половины
+                mergeSort(array, left, mid);
+                mergeSort(array, mid + 1, right);
+                // Перенос отсортированных половин в метод merge
+                merge(array, left, mid, right);
+            }
+    }
+    // Метод для слияния двух подмассивов
+    private void merge(int[] array, int left, int mid, int right) {
+        // Определение размеров подмассивов
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        // Создание временных подмассивов
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+        // Копирование данных во временные массивы
+        for (int i = 0; i < n1; i++)
+            leftArray[i] = array[left + i];
+        for (int j = 0; j < n2; j++)
+            rightArray[j] = array[mid + 1 + j];
+        // Перенос временных массивов обратно в array
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i];
+                i++;
+            } else {
+                array[k] = rightArray[j];
+                j++;
+        }
+        k++;
+        }
+        // Перенос отсавшихся элементов, если такие есть
+        while (i < n1) {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
         InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson04/dataB.txt");
