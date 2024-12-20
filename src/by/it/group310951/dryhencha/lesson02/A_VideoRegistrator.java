a4a480919e3aec2189d25e96528fcb8412eb6327
Rivelon <rivelon22@gmail.com>
2024-12-20 22:23:37 +0300

    lesson2a

---
+++ src/by/it/group310951/dryhencha/lesson02/A_VideoRegistrator.java
@@ -0,0 +1,47 @@
+package by.it.group310951.dryhencha.lesson02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/*
+даны события events
+реализуйте метод calcStartTimes, так, чтобы число включений регистратора на
+заданный период времени (1) было минимальным, а все события events
+были зарегистрированы.
+Алгоритм жадный. Для реализации обдумайте надежный шаг.
+*/
+
+public class A_VideoRegistrator {
+
+    public static void main(String[] args)  {
+        A_VideoRegistrator instance=new A_VideoRegistrator();
+        double[] events=new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
+        List<Double> starts=instance.calcStartTimes(events,1); //рассчитаем моменты старта, с длинной сеанса 1
+        System.out.println(starts); //момент старта
+    }
+    //модификаторы доступа опущены для возможности тестирования
+    public List<Double> calcStartTimes(double[] events, double workDuration)  {
+        //events - события которые нужно зарегистрировать
+        //timeWorkDuration время работы видеокамеры после старта
+        List<Double> result;
+        //hint: сортировка Arrays.sort обеспечит скорость алгоритма
+        //C*(n log n) + C1*n = O(n log n)
+        Arrays.sort(events);
+        result = new ArrayList<>();
+        result.add(events[0]);
+        // время старта видеокамеры
+        double lastEvent = events[0];
+
+        for (int i = 1; i < events.length; i++) {
+            //System.out.printf("A_VideoRegistrator event = %f\n", events[i]);
+            //System.out.printf("A_VideoRegistrator lastEvent + workDuration = %f\n", lastEvent + workDuration);
+            if (events[i] > lastEvent + workDuration) {  //вычислим момент окончания работы видеокамеры
+                //System.out.println("A_VideoRegistrator RESET event");
+                result.add(events[i]);
+                lastEvent = events[i];
+            }
+        }
+
+        return result; //вернем итог
+    }
+}
