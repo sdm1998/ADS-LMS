66d532c20050b09612fee3e1a614f5aadebce8d7
Porepko Kristina <porepkokristina2005@gmail.com>
2024-12-25 20:44:25 +0300

    lesson1 fiboA

--- src/by/it/group310951/porepko/lesson01/FiboB.java
+++ src/by/it/group310951/porepko/lesson01/FiboB.java
@@ -20,7 +20,7 @@ public class FiboB {
         //вычисление чисел простым быстрым методом
 
         FiboB fibo = new FiboB();
-        int n = 55555;
+        int n = 333;
 
 
         System.out.printf("fastB(%d)=%d \n\t time=%d \n\n", n, fibo.fastB(n), fibo.time());
@@ -29,8 +29,19 @@ public class FiboB {
     BigInteger fastB(Integer n) {
         //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
 
-            return BigInteger.ZERO;
+        if (n == 1) return BigInteger.ONE;
+        else if (n == 0) return BigInteger.ZERO;
+        else {
+            BigInteger[] fibArray;
+            fibArray = new BigInteger[n + 1];
+            fibArray[0] = BigInteger.ZERO;
+            fibArray[1] = BigInteger.ONE;
+            for (int i = 2; i <= n; i++) {
+                fibArray[i] = fibArray[i - 1].add(fibArray[i - 2]);
+
+
+            }
+            return fibArray[n];
+        }
     }
-
 }
-
