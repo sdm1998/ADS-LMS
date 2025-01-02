4ae697f1d40782ea1957427fe5de9d52ee63c0a8
Porepko Kristina <porepkokristina2005@gmail.com>
2025-01-02 18:29:23 +0300

    lesson1 FiboC

--- src/by/it/group310951/porepko/lesson01/FiboC.java
+++ src/by/it/group310951/porepko/lesson01/FiboC.java
@@ -6,6 +6,8 @@ package by.it.group310951.porepko.lesson01;
  * время расчета должно быть не более 2 секунд
  */
 
+import java.math.BigInteger;
+
 public class FiboC {
 
     private long startTime = System.currentTimeMillis();
@@ -16,18 +18,55 @@ public class FiboC {
 
     public static void main(String[] args) {
         FiboC fibo = new FiboC();
-        int n = 1000;
-        int m = 55;
+        int n = 999999999;
+        int m = 321;
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
 
-    long fasterC(long n, int m) {
-        //Решение сложно найти интуитивно
-        //возможно потребуется дополнительный поиск информации
-        //см. период Пизано
-        return 0L;
+    private int getPisanoPeriod(int m) {
+
+        // вычисление длины периода последовательности Фибоначчи
+        // по модулю заданного числа m
+        int current = 0;
+        int next = 1;
+        int period = 0;
+        while (true) {
+            int oldNext = next;
+            next = (current + next) % m;
+            current = oldNext;
+            period ++;
+            if (current == 0 && next ==1) {
+                return period;
+            }
+        }
     }
 
+    long fasterC(long n, int m) {
+        if (n == 0L) return 0L;
+        else if (n == 1L) return 1L;
+        else {
 
+            // вычисляем период Пизано по заданному m
+            int period = getPisanoPeriod(m);
+
+            // вычисляем минимальный элемент последовательности Фибоначчи,
+            // который даст тот же остаток, что и n
+
+            long result = n % period;
+
+            // вычисляем значение найденного на предыдущем шаге элемента
+
+            FiboB fibo = new FiboB();
+            BigInteger fiboResult = fibo.fastB(Math.toIntExact(result));
+
+            // получаем искомый остаток
+
+            long mod = fiboResult.longValue() % m;
+
+            return mod;
+        }
+    }
 }
+
+
 
