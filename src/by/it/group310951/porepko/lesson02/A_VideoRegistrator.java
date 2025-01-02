c64e1c2df6b79840efd2001cfdda9ab32c1654da
Porepko Kristina <porepkokristina2005@gmail.com>
2025-01-02 18:46:55 +0300

    lesson1 A_VideoRegistrator

--- src/by/it/group310951/porepko/lesson02/A_VideoRegistrator.java
+++ src/by/it/group310951/porepko/lesson02/A_VideoRegistrator.java
@@ -1,7 +1,8 @@
-package by.it.group310951.porepko.lesson01.lesson02;
+package by.it.group310951.porepko.lesson02;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 /*
 даны события events
 реализуйте метод calcStartTimes, так, чтобы число включений регистратора на
@@ -18,25 +19,34 @@ public class A_VideoRegistrator {
         List<Double> starts=instance.calcStartTimes(events,1); //рассчитаем моменты старта, с длинной сеанса 1
         System.out.println(starts);                            //покажем моменты старта
     }
-    //модификаторы доступа опущены для возможности тестирования
+    //модификаторы доступа опущены для возможности тестированиb
+
     List<Double> calcStartTimes(double[] events, double workDuration)  {
+
         //events - события которые нужно зарегистрировать
         //timeWorkDuration время работы видеокамеры после старта
         List<Double> result;
-        result = new ArrayList<>();
-        int i=0;                              //i - это индекс события events[i]
-        //комментарии от проверочного решения сохранены для подсказки, но вы можете их удалить.
-        //подготовка к жадному поглощению массива событий
+
+
         //hint: сортировка Arrays.sort обеспечит скорость алгоритма
         //C*(n log n) + C1*n = O(n log n)
 
-        //пока есть незарегистрированные события
-        //получим одно событие по левому краю
-        //и запомним время старта видеокамеры
-        //вычислим момент окончания работы видеокамеры
-        //и теперь пропустим все покрываемые события
-        //за время до конца работы, увеличивая индекс
+        Arrays.sort(events);
+        result = new ArrayList<>();
+        result.add(events[0]);
 
+        // время старта видеокамеры
+        double lastEvent = events[0];
+
+        for (int i = 1; i < events.length; i++) {
+            //System.out.printf("A_VideoRegistrator event = %f\n", events[i]);
+            //System.out.printf("A_VideoRegistrator lastEvent + workDuration = %f\n", lastEvent + workDuration);
+            if (events[i] > lastEvent + workDuration) {  //вычислим момент окончания работы видеокамеры
+
+                result.add(events[i]);
+                lastEvent = events[i];
+            }
+        }
 
 
         return result;                        //вернем итог
