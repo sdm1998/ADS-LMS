3cf085830feddd00b1aee4db0665a39dfa2db7b9
Porepko Kristina <porepkokristina2005@gmail.com>
2025-01-03 22:58:52 +0300

    lesson 1

--- src/by/it/group310951/porepko/lesson01/FiboB.java
+++ src/by/it/group310951/porepko/lesson01/FiboB.java
@@ -18,7 +18,6 @@ public class FiboB {
     public static void main(String[] args) {
 
         //вычисление чисел простым быстрым методом
-
         FiboB fibo = new FiboB();
         int n = 55555;
 
