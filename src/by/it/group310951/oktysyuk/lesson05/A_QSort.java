83b58a5d326cf642ac25e5504e043cbc01d8f8c8
oktysyuk <m.oktysyuk@gmail.com>
2025-01-13 15:55:55 +0300

    lesson 5 A,B,C

---
+++ src/by/it/group310951/oktysyuk/lesson05/A_QSort.java
@@ -0,0 +1,132 @@
+package by.it.group310951.oktysyuk.lesson05;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+
+/*
+Видеорегистраторы и площадь.
+На площади установлена одна или несколько камер.
+Известны данные о том, когда каждая из них включалась и выключалась (отрезки работы)
+Известен список событий на площади (время начала каждого события).
+Вам необходимо определить для каждого события сколько камер его записали.
+
+В первой строке задано два целых числа:
+    число включений камер (отрезки) 1<=n<=50000
+    число событий (точки) 1<=m<=50000.
+
+Следующие n строк содержат по два целых числа ai и bi (ai<=bi) -
+координаты концов отрезков (время работы одной какой-то камеры).
+Последняя строка содержит m целых чисел - координаты точек.
+Все координаты не превышают 10E8 по модулю (!).
+
+Точка считается принадлежащей отрезку, если она находится внутри него или на границе.
+
+Для каждой точки в порядке их появления во вводе выведите,
+скольким отрезкам она принадлежит.
+    Sample Input:
+    2 3
+    0 5
+    7 10
+    1 6 11
+    Sample Output:
+    1 0 0
+
+*/
+
+public class A_QSort {
+
+    //отрезок
+    private class Segment  implements Comparable<Segment>{
+        int start;
+        int stop;
+
+        Segment(int start, int stop){
+            this.start = start;
+            this.stop = stop;
+            //тут вообще-то лучше доделать конструктор на случай если
+            //концы отрезков придут в обратном порядке
+        }
+
+        @Override
+        public int compareTo(Segment o) {
+            //подумайте, что должен возвращать компаратор отрезков
+
+            return 0;
+        }
+    }
+
+
+    int[] getAccessory(InputStream stream) throws FileNotFoundException {
+        //подготовка к чтению данных
+        Scanner scanner = new Scanner(stream);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        //число отрезков отсортированного массива
+        int n = scanner.nextInt();
+        Segment[] segments=new Segment[n];
+        //число точек
+        int m = scanner.nextInt();
+        int[] points=new int[m];
+        int[] result=new int[m];
+
+        //читаем сами отрезки
+        for (int i = 0; i < n; i++) {
+            //читаем начало и конец каждого отрезка
+            segments[i]=new Segment(scanner.nextInt(),scanner.nextInt());
+        }
+        //читаем точки
+        for (int i = 0; i < m; i++) {
+            points[i]=scanner.nextInt();
+        }
+        //тут реализуйте логику задачи с применением быстрой сортировки
+        //в классе отрезка Segment реализуйте нужный для этой задачи компаратор
+        // сортировка отрезков
+        Arrays.sort(segments);
+
+        // массив для точек и их индексов
+        int[][] indexedPoints = new int[m][2];
+        for (int i = 0; i < m; i++) {
+            indexedPoints[i][0] = points[i];
+            indexedPoints[i][1] = i;
+        }
+
+        // сортировка точек
+        Arrays.sort(indexedPoints, Comparator.comparingInt(a -> a[0]));
+
+        // подсчет камер для каждой точки
+        int j = 0; // указатель на отрезки
+        for (int i = 0; i < m; i++) {
+            int point = indexedPoints[i][0];
+            while (j < n && segments[j].stop < point) {
+                j++; // пропуск отрезков, которые закончились до точки
+            }
+            int count = 0;
+            for (int k = j; k < n; k++) {
+                if (segments[k].start <= point && segments[k].stop >= point) {
+                    count++; // подсчет отрезков, которые охватывают точку
+                } else {
+                    break;
+                }
+            }
+            result[indexedPoints[i][1]] = count; // запись результата по индексу точки
+        }
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return result;
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson05/dataA.txt");
+        A_QSort instance = new A_QSort();
+        int[] result=instance.getAccessory(stream);
+        for (int index:result){
+            System.out.print(index+" ");
+        }
+    }
+
+}
